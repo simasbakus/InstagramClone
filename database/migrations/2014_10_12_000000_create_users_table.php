67895b0c4f97c6613c8bc94e25fbd2efa4f02722
simasbakus <58630470+simasbakus@users.noreply.github.com>
2020-02-16 10:48:49 +0200

    adding default profile pic and username

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,10 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profilePic')->default('defaultProfilePic.png');
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    {{-- @if (session('status'))
+                    @endif --}}
+                    <div class="row">
+                      <img src="{{ asset('storage/uploads/' . Auth::user()->profilePic) }}" alt="" style="width:150px; height:150px">
+                    </div>
 
-                    You are logged in!
-                </div>
-            </div>
         </div>
     </div>
 </div>
